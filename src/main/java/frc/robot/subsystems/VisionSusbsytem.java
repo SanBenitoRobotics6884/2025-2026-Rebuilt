adb4b446fe79a920fe0d5d3b772d7a43cb5a8933
deep-space-robotics <dsr@sbrobotics.org>
2026-02-06 18:30:06 -0800

    create photoncamera, get unreadresults in list and obtain best target

---
+++ src/main/java/frc/robot/subsystems/VisionSusbsytem.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSusbsytem extends SubsystemBase {
+  PhotonCamera m_randomAssCamera = new PhotonCamera("HD_USB_CAMERA");
+  List<PhotonPipelineResult> unreadresults = m_randomAssCamera.getAllUnreadResults();
+  PhotonTrackedTarget target = m_randomAssCamera.getAllUnreadResults().get(0).getBestTarget();
+  
+
+  /** Creates a new VisionSusbsytem. */
+  public VisionSusbsytem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
