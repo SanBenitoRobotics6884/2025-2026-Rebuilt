4cba2874ebd7cd026d522e1c98c214de76f0deea
Zachary Lozano <lozanozachary13@gmail.com>
2026-02-07 21:08:15 -0800

    get values, put in smartdashboard

--- src/main/java/frc/robot/subsystems/VisionSusbsytem.java
+++ src/main/java/frc/robot/subsystems/VisionSusbsytem.java
@@ -4,20 +4,25 @@
 
 package frc.robot.subsystems;
 
-import java.lang.reflect.UndeclaredThrowableException;
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSusbsytem extends SubsystemBase {
   PhotonCamera m_randomAssCamera = new PhotonCamera("HD_USB_CAMERA");
-  List<PhotonPipelineResult> unreadResults = m_randomAssCamera.getAllUnreadResults();
-  PhotonTrackedTarget bestTarget = unreadResults.get(0).getBestTarget();
-  
+  //PhotonTrackedTarget bestTarget = unreadResults.get(0).getBestTarget();
+
+  boolean AprilTagSight = false;
+
+  double targetYaw;
+  double targetPitch;
+  double targetSkew;
+  double targetArea;
 
   /** Creates a new VisionSusbsytem. */
   public VisionSusbsytem() {
@@ -26,14 +31,28 @@ public class VisionSusbsytem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    
+    List<PhotonPipelineResult> unreadResults = m_randomAssCamera.getAllUnreadResults();
+    //Checks to see if sigma awesome unread results is not empty.
     if (!unreadResults.isEmpty()) {
+      //Always sees the latest entry in the pipeline.
       var result = unreadResults.get(unreadResults.size() - 1);
       if (result.hasTargets()) {
         for (var target : result.getTargets()) {
-          
+          targetYaw = target.getYaw();
+          targetPitch = target.getPitch();
+          targetSkew = target.getSkew();
+          targetArea = target.getArea();
+
         }
       }
     }
-    // This method will be called once per scheduler run
+
+    SmartDashboard.putBoolean("April Tag", AprilTagSight);
+    SmartDashboard.putNumber("Yaw:", targetYaw);
+    SmartDashboard.putNumber("Pitch", targetPitch);
+    SmartDashboard.putNumber("Skew", targetSkew);
+    SmartDashboard.putNumber("Yaw:", targetArea);
+
   }
 }
