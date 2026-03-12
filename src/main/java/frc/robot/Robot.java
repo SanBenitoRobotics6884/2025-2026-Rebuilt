e4ddb8197008b07b177786988120ade185630c16
Conley Shemanski <captaintate137@gmail.com>
2026-03-11 20:46:33 -0700

    added drive camera (untested)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,12 +7,19 @@ package frc.robot;
 import com.ctre.phoenix6.HootAutoReplay;
 import com.pathplanner.lib.auto.AutoBuilder;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class Robot extends TimedRobot {
+
+    @Override
+    public void robotInit() {
+        // Start the camera server with default settings
+        CameraServer.startAutomaticCapture();
+    }
 
     private Command m_autonomousCommand;
 
